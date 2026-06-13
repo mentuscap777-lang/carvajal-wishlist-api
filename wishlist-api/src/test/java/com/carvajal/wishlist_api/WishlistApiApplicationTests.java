35d978631dd06d7b36351711ef32d81415351760
mentuscap777-lang <nunakay777@gmail.com>
2026-06-13 09:49:35 -0500

    feat: Carvajal Wishlist API - Entities, Controllers y Repositories

---
+++ wishlist-api/src/test/java/com/carvajal/wishlist_api/WishlistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carvajal.wishlist_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishlistApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
